184b8b178c03b6a26557a99c68ed53edbc309865
Narges Safari <95274071+nargesazad@users.noreply.github.com>
2022-03-31 18:56:39 +0430

    make forecast part  and link to my shecodes page

--- src/index.js
+++ src/index.js
@@ -24,9 +24,58 @@ function formatDate() {
 }
 let time = document.querySelector("div#time");
 time.innerHTML = formatDate();
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return day;
+}
 //end of time section
 
-//api part
+//forcastApi part
+
+function forcasting(response) {
+  let forcasts = response.data.daily;
+
+  let forcastElement = document.querySelector("#forcasts");
+
+  let forcastHtml = `<div class="row">`;
+
+  forcasts.forEach(function (forcastDay, index) {
+    if (index < 6) {
+      forcastHtml =
+        forcastHtml +
+        `<div class="col-2">
+                   <div class="card" >
+  <div class="card-body">
+    <p class="card-text">${formatDay(forcastDay.dt)}</p>
+    <h5 class="card-title"><img src="http://openweathermap.org/img/wn/${
+      forcastDay.weather[0].icon
+    }@2x.png" width="30px" /></h5>
+    <h6 class="card-subtitle mb-2 text-muted" >${Math.round(
+      forcastDay.temp.day
+    )}<span class = "currentUnit">°C</span></h6>
+  </div>
+</div>
+</div>
+`;
+    }
+  });
+  forcastHtml = forcastHtml + `</div>`;
+  forcastElement.innerHTML = forcastHtml;
+}
+
+function getForcastApi(coordinat) {
+  let lat = coordinat.lat;
+  let lon = coordinat.lon;
+  let apiKey = "90752b41f27333ec27018bf17cc38b4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(forcasting);
+}
+//end of forcastApi part
+
+//currenttApi part
 function showTemp(response) {
   let currentTemp = document.querySelector("#current-temp");
   currentTemp.innerHTML = Math.round(response.data.main.temp);
@@ -38,32 +87,45 @@ function showTemp(response) {
   feels.innerHTML = Math.round(response.data.main.feels_like);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
-  let icon = response.data.weather[0].description;
+  getForcastApi(response.data.coord);
+  let icon = response.data.weather[0].icon;
   //imag part
   let stat = "sun";
   switch (icon) {
-    case `snow`:
+    case `13d`:
       stat = "snow";
       break;
-    case `few clouds`:
+    case `13n`:
+      stat = "snow";
+      break;
+    case `03d`:
       stat = "cloud";
       break;
-    case `scattered clouds`:
+    case `03n`:
       stat = "cloud";
       break;
-    case `broken clouds`:
+    case `04n`:
       stat = "cloud";
       break;
-    case `overcast clouds`:
+    case `04d`:
       stat = "cloud";
       break;
-    case `shower rain`:
+    case `09d`:
       stat = "rain";
       break;
-    case `rain`:
+    case `10d`:
       stat = "rain";
       break;
-    case `thunderstorm`:
+    case `11d`:
+      stat = "rain";
+      break;
+    case `09n`:
+      stat = "rain";
+      break;
+    case `10n`:
+      stat = "rain";
+      break;
+    case `11n`:
       stat = "rain";
       break;
     default:
@@ -80,7 +142,6 @@ function searchingCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
 }
-//api part
 //search city part
 function submiting(event) {
   event.preventDefault();
@@ -92,3 +153,4 @@ let search = document.querySelector("#search-city");
 search.addEventListener("submit", submiting);
 searchingCity("tehran");
 //end of search city part
+//endofcurrentApi part
